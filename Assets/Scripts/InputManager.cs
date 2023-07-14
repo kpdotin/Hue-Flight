72dbcfdefad30f4d054e9847c8e9db1130ca1413
Karthikeyan Pandiyan <karthikpandiyan2712@gmail.com>
2023-07-14 18:46:18 +0530

    Spiral Included Level Design Test

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private TouchControls playerControls;
+
+    #region EVENTS
+    public delegate void TouchStarted();
+    public event TouchStarted OnTouchStarted;
+
+    public delegate void TouchDetected(Vector2 pos);
+    public event TouchDetected OnTouchDetected;
+
+    public delegate void TouchCanceled();
+    public event TouchCanceled OnTouchCanceled;
+
+    public delegate void RightTap();
+    public event RightTap OnRightTap;
+
+    public delegate void LeftTap();
+    public event LeftTap OnLeftTap;
+
+    #endregion
+
+    private void Awake() => playerControls = new TouchControls();
+    private void OnEnable()
+    {
+        playerControls.Enable();
+        OnTouchDetected += DetectMovement;
+    }
+    private void OnDisable()
+    {
+        playerControls.Disable();
+        OnTouchDetected -= DetectMovement;
+    }
+
+    private void Start()
+    {
+        playerControls.Touch.Contact.started += ctx => TouchStart(ctx);
+        playerControls.Touch.Contact.performed += ctx => TouchDetect(ctx);
+        playerControls.Touch.Contact.canceled += ctx => TouchCancel(ctx);
+    }
+
+    void TouchStart(InputAction.CallbackContext ctx) { OnTouchStarted?.Invoke(); }
+    void TouchDetect(InputAction.CallbackContext ctx){ OnTouchDetected?.Invoke(ScreenPosition()); }
+    void TouchCancel(InputAction.CallbackContext ctx) { OnTouchCanceled?.Invoke(); }
+    public Vector2 ScreenPosition() { return playerControls.Touch.Position.ReadValue<Vector2>(); }
+
+    void DetectMovement(Vector2 pos)
+    {
+        Debug.Log(pos);
+        if(pos.x < 541f) { OnLeftTap?.Invoke(); }
+        if(pos.x > 540f) { OnRightTap?.Invoke(); }
+    }
+}
--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -6,28 +6,74 @@ public class MovementScript : MonoBehaviour
 {
     [SerializeField] float movementSpeed;
     [SerializeField] float forwardSpeed;
+    private InputManager inputManager;
     Rigidbody rb;
+    [SerializeField] bool rightTap = false;
+    [SerializeField] bool leftTap = false;
+    [SerializeField] bool canceled = true;
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        inputManager = GetComponent<InputManager>();
     }
+    private void OnEnable()
+    {
+        inputManager.OnTouchStarted += TouchStarted;
+        inputManager.OnRightTap += OnRightTap;
+        inputManager.OnLeftTap += OnLeftTap;
+        inputManager.OnTouchCanceled += TouchCanceled;
+    }
+    private void OnDisable()
+    {
+        inputManager.OnTouchStarted -= TouchStarted;
+        inputManager.OnRightTap -= OnRightTap;
+        inputManager.OnLeftTap -= OnLeftTap;
+        inputManager.OnTouchCanceled -= TouchCanceled;
+    }
+
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.LeftArrow)) 
-        { 
-            rb.velocity = transform.TransformDirection( new Vector3(movementSpeed * (-1),0,movementSpeed));
-            rb.AddRelativeForce(Vector3.forward * forwardSpeed,ForceMode.VelocityChange);
+        if(!canceled)
+        {
+            if (leftTap)
+            {
+                rb.velocity = transform.TransformDirection(new Vector3(movementSpeed * (-1), 0, movementSpeed));
+                rb.AddRelativeForce(Vector3.forward * forwardSpeed, ForceMode.VelocityChange);
+                
+            }
+
+
+            if (rightTap)
+            {
+                rb.velocity = transform.TransformDirection(new Vector3(movementSpeed, 0, movementSpeed));
+                rb.AddRelativeForce(Vector3.forward * forwardSpeed, ForceMode.VelocityChange);
+                
+
+            }
         }
+    }
 
 
-        if (Input.GetKey(KeyCode.RightArrow)) 
-        { 
-            rb.velocity = transform.TransformDirection( new Vector3(movementSpeed,0,movementSpeed));
-            rb.AddRelativeForce(Vector3.forward * forwardSpeed, ForceMode.VelocityChange);
+    void OnRightTap()
+    {
+        rightTap = true;
+    }
 
-        }
+    void OnLeftTap()
+    {
+        leftTap = true;
+    }
+    void TouchStarted()
+    {
+         canceled = false;
+    }
+    void TouchCanceled()
+    {
+        rightTap = false;
+        leftTap = false;
+        canceled = true;
     }
 }
