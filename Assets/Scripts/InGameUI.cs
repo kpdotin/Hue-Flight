d1ecd1939a47633919735589c552e196216e6dbb
Karthikeyan Pandiyan <karthikpandiyan2712@gmail.com>
2023-07-26 18:20:33 +0530

    Shader Graph Update

---
+++ Assets/Scripts/InGameUI.cs
@@ -0,0 +1,29 @@
+using TMPro;
+using UnityEngine;
+
+public class InGameUI : MonoBehaviour
+{
+    public delegate void CoinCollected();
+    public CoinCollected OnCoinCollected;
+
+    int coinsCollected;
+    public TextMeshProUGUI coinsText;
+    private void OnEnable()
+    {
+        OnCoinCollected += CoinUpdater;
+    }
+    private void OnDisable()
+    {
+        OnCoinCollected -= CoinUpdater;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        coinsText.text = "Coins : " + coinsCollected.ToString();
+    }
+
+    void CoinUpdater()
+    {
+        coinsCollected++;
+    }
+}
